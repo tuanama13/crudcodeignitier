1e96890047ef0cf8dbf7b4ff0b1e8ec5bc628f06
tuanama13 <tuanama7@gmail.com>
2018-08-07 22:10:43 +0700

    addpost

--- application/controllers/Posts.php
+++ application/controllers/Posts.php
@@ -10,4 +10,18 @@
 			$this->load->view('posts/index', $data);
 			$this->load->view('templates/footer');
 		}
+
+		public function view($slug= NULL)
+		{
+			$data['post'] = $this->post_model->get_posts($slug);
+
+			if (empty($data['post'])) {
+				show_404();
+			}
+
+			$data['title']=$data['post']['title'];
+			$this->load->view('templates/header');
+			$this->load->view('posts/view', $data);
+			$this->load->view('templates/footer');
+		}
 	}
--- application/views/posts/index.php
+++ application/views/posts/index.php
@@ -3,4 +3,5 @@
 	<h3><?php echo $post['title']; ?></h3>
 	<small>Posted on : <?php echo $post['created_at']; ?> </small><br>
 	<?php echo $post['body']; ?>
+	<a href="<?php echo site_url('/posts/'.$post['slug']); ?>">Read More</a>
 <?php endforeach; ?>
